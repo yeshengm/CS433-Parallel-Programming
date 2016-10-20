4347057d7d358ccd4c59e50a0bc7745c9147e537
cktonychang@gmail.com <cktonychang@gmail.com>
2016-10-21 00:13:03 +0800

    Done

--- PA1/test.sh
+++ PA1/test.sh
@@ -1,6 +1,7 @@
 # ./dijkstra < ./data/$1.txt
 make
-data="/home/manifold/Desktop/dat/${1}.txt"
+data="/Users/changke/Desktop/data/${1}.txt"
+#data="/home/manifold/Desktop/dat/${1}.txt"
 echo Running on $data with $2 threads
 mpiexec -n $2 ./mpi_io $data
 

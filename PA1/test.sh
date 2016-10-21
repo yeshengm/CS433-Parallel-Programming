87cdff5495c9e9e6d8455d7034a931eca9913372
manifold <mayesheng@sjtu.edu.cn>
2016-10-21 20:36:33 +0800

    PA1 finished

--- PA1/test.sh
+++ PA1/test.sh
@@ -1,7 +1,7 @@
-# ./dijkstra < ./data/$1.txt
-make
-#data="/Users/changke/Desktop/data/${1}.txt"
-data="/home/manifold/Desktop/dat/${1}.txt"
-echo Running on $data with $2 threads
-mpiexec -n $2 ./mpi_io $data
+# make
+./dijkstra < ~/Desktop/dat/$1.txt
+# data="/Users/changke/Desktop/data/${1}.txt"
+# data="/home/manifold/Desktop/dat/${1}.txt"
+# echo Running on $data with $2 threads
+# mpiexec -n $2 ./mpi_io $data
 
